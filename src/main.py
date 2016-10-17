b5fc3fc3dc04d74ad38c066a56bc002a06864384
Oleg Sushkov <osushkov@gmail.com>
2016-10-17 21:33:57 +1100

    split the idx parsing into a separate module

--- src/main.py
+++ src/main.py
@@ -1,61 +1,9 @@
-import struct
-import numpy as np
 
-def readIdxInt(file):
-    data = file.read(4)
-    return struct.unpack(">i", data)[0]
-
-
-def readImages(path):
-    with open(path, "rb") as f:
-        mn = readIdxInt(f)
-        if mn != 2051:
-            print("File did not contain expected magic number")
-            return None
-
-        numEntries = readIdxInt(f)
-        imgWidth = readIdxInt(f)
-        imgHeight = readIdxInt(f)
-
-        if imgWidth <= 0 or imgHeight <= 0 or numEntries <= 0:
-            return None
-
-        vectorDim = imgWidth * imgHeight
-        result = np.empty((numEntries, vectorDim))
-
-        for e in xrange(numEntries):
-            for i in xrange(vectorDim):
-                pixelBuf = f.read(1)
-                result[e, i] = struct.unpack("B", pixelBuf)[0] / 255.0
-
-        print(numEntries)
-
-
-def readLabels(path):
-    with open(path, "rb") as f:
-        mn = readIdxInt(f)
-        if mn != 2049:
-            print("File did not contain expected magic number")
-            return None
-
-        numEntries = readIdxInt(f)
-
-        result = np.empty((numEntries, 10))
-        result.fill(0.0)
-
-        for e in xrange(numEntries):
-            digitBuf = f.read(1)
-            digit = struct.unpack("B", digitBuf)[0]
-            assert(digit >= 0 and digit <= 9)
-
-            result[e][digit] = 1.0
-
-        print(result)
-        print(numEntries)
+import idx
 
 a = [1, 2, 3]
 print("hello world")
 print(a)
 
-readLabels("data/test_labels.idx1")
-readImages("data/test_images.idx3")
+idx.readLabels("data/test_labels.idx1")
+idx.readImages("data/test_images.idx3")
