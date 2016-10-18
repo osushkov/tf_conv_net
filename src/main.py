fae87d45992cf47f6a4a5808ccc7e9f6b3c15be6
Oleg Sushkov <sushkov@gmail.com>
2016-10-18 18:12:08 +1100

    mini batch learning, train and test on separate datasets

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 
 import idx
 import tensorflow as tf
+import numpy as np
 
-labels = idx.readLabels("data/test_labels.idx1")
-images = idx.readImages("data/test_images.idx3")
+def loadInputOutput(inputsPath, outputsPath):
+  inputs = idx.readImages(inputsPath)
+  outputs = idx.readLabels(outputsPath)
+
+  assert(inputs.shape[0] == outputs.shape[0])
+
+  permutation = np.random.permutation(inputs.shape[0])
+  return (inputs[permutation,:], outputs[permutation,:])
+
+trainImages, trainLabels = loadInputOutput("data/train_images.idx3", "data/train_labels.idx1")
+testImages, testLabels = loadInputOutput("data/test_images.idx3", "data/test_labels.idx1")
 
 sess = tf.Session()
 
@@ -22,11 +32,19 @@ accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
 sess.run(tf.initialize_all_variables())
 
+BATCH_SIZE = 100
 for i in range(1000):
-    _, loss_value = sess.run([trainStep, crossEntropy], feed_dict={input:images, target:labels})
+    offset = (i * BATCH_SIZE) % (trainImages.shape[0] - BATCH_SIZE)
+
+    feedDict = {
+        input : trainImages[offset:offset+BATCH_SIZE],
+        target : trainLabels[offset:offset+BATCH_SIZE],
+    }
+
+    _, loss_value = sess.run([trainStep, crossEntropy], feed_dict=feedDict)
     print("loss: %f" % loss_value)
 
-trainAccuracy = sess.run(accuracy, feed_dict={input:images, target:labels})
+trainAccuracy = sess.run(accuracy, feed_dict={input:testImages, target:testLabels})
 print("train accuracy: %f" % trainAccuracy)
 
 
